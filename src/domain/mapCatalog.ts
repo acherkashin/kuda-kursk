46366254f0fe2743df9b3a7ec6ba1c60458fa3bc
Cherkashin Alexander <cherkalexander@gmail.com>
2026-06-16 22:51:59 +0300

    Shorten Elena Koltysheva map description

--- src/domain/mapCatalog.ts
+++ src/domain/mapCatalog.ts
@@ -31,7 +31,7 @@ export const mapCatalog = [
   {
     slug: "elena-koltysheva",
     title: "Елена Колтышева",
-    description: "Подборка мест для впечатлений в Курске и области",
+    description: "Подборка мест для впечатлений",
     logo: "/place-thumbnails/elena-koltysheva/white-square.webp",
     dataPath: "/data/elena-koltysheva-objects.json"
   }
